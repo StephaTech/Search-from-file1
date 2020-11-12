594eac3dd530a2c4ee2916a665f26b9d2d2c29c7
StephaTech <stephatech123@gmail.com>
2020-11-12 17:32:30 +0000

    class update

--- src/Search/Record.java
+++ src/Search/Record.java
@@ -1,5 +1,53 @@
 package Search;
+/**
+*
+* @author apont
+*/
 
 public class Record {
+	private int id;
+    private String name;
+    private String surname;
+    private String gender;
+    private String country;
+    private int age;
+    private String date;
+    
+    public Record(int id, String name, String surname, String gender, String country, int age, String date) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.gender = gender;
+        this.country = country;
+        this.age = age;
+        this.date = date;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getDate() {
+        return date;
+    }
 }
--- src/Search/SearchFromFile.java
+++ src/Search/SearchFromFile.java
@@ -1,10 +1,75 @@
 package Search;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+
 public class SearchFromFile {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		new SearchFromFile();
 
 	}
-
+	public SearchFromFile(){
+        
+	       ArrayList<Record> records = loadData();
+	       System.out.println(linearSearch(records, "Douglas")); 
+	    }
+	    
+	    public ArrayList<Record> loadData() {
+	        
+	        ArrayList<Record> records = new ArrayList<>();
+	        
+	        try {
+	            BufferedReader br = new BufferedReader(new FileReader("bookInfo.txt"));
+	            String contentLine = br.readLine();
+	            
+	            String[] data;
+	            int id;
+	            String name;
+	            String surname;
+	            String gender;
+	            String country;
+	            int age;
+	            String date;
+	            
+	            while (contentLine != null) {
+	                
+	                data = contentLine.split(",");
+	                id = Integer.parseInt(data[0]);
+	                name = data[1];
+	                surname = data[2];
+	                gender = data[3];
+	                country = data[4];
+	                age = Integer.parseInt(data[5]);
+	                date = data[6];
+	                records.add(new Record(id, name, surname, gender, country, age, date));
+	                contentLine = br.readLine();
+	            }
+	            
+	        } catch (IOException ex) {
+	            Logger.getLogger(SearchFromFile.class.getName()).log(Level.SEVERE, null, ex);
+	        }
+	        
+	        return records;
+	    }
+	    
+	    public int linearSearch(ArrayList<Record> array, String targetName){
+	        
+	        // Going one by one the elements in the array
+	        for(int i = 0; i < array.size(); i++){
+	            System.out.println("Testing: " + array.get(i).getName());
+	            // When the element is found, stop the loop and return the index
+	            if(array.get(i).getName().equals(targetName)){
+	                return i;
+	            }            
+	        }
+	        return -1;
+	    }
 }
